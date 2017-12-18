7e27dbf00a58b31a62ca81428bead09d74c10f50
elimenchaca <menchacaeli@gmail.com>
2017-12-18 14:21:20 -0700

    Added Geolocation and Orientation

--- src/sensorplug.js
+++ src/sensorplug.js
@@ -2,11 +2,27 @@
  * Created by elimenchaca on 11/6/17.
  */
 var Device = (function IIFE() {
-    var detectOrientation = function () {
-        if (!('ondeviceorientation' in window)) {
-            console.log("Device Orientation is not detected");
+    var detectAcceleration = function() {
+        if (!('ondevicemotion' in window)) {
+            return "sensorplug: Device Acceleration is not supported";
         } else {
-            console.log("Device Orientation detected")
+            return "sensorplug: Device Acceleration is supported!";
+        }
+    };
+
+    var detectOrientation = function() {
+        if (!('ondeviceorientation' in window)) {
+            return "sensorplug: Device Orientation is not supported";
+        } else {
+            return "sensorplug: Device Orientation is supported!" ;
+        }
+    };
+
+    var detectGeolocation = function() {
+        if (!("geolocation" in navigator)) {
+            return "sensorplug: Device Geolocation is not supported.";
+        } else {
+            return "sensorplug: Device Geolocation is supported!";
         }
     };
 
@@ -19,15 +35,63 @@ var Device = (function IIFE() {
             // alpha is the compass direction the device is facing in degrees
             var alpha = eventData.alpha;
         
-            var obj = {"gamma": gamma, "beta": beta, "alpha" : alpha};
-            socket.emit('orientation', obj);
+            var oriObj = {"gamma": gamma, "beta": beta, "alpha" : alpha};
+            socket.emit('orientation', oriObj);
         
         }, false);
     };
 
+    var handleGeolocation = function() {
+        var positionOptions = {
+            enableHighAccuracy: true,
+            timeout: 10 * 1000, // 10 seconds
+            maximumAge: 30 * 1000 // 30 seconds
+        };
+
+        function success(position) {
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+
+            var geoObj = {"latitude": latitude, "longitude": longitude};
+            socket.emit('geolocation', geoObj);
+        }
+
+        function error() {
+            console.log("error: Check sensorplug documentation");
+        }
+
+        navigator.geolocation.getCurrentPosition(success, error, positionOptions);
+    };
+
+    var handleAcceleration = function() {
+        if (window.DeviceMotionEvent) {
+            window.addEventListener('devicemotion', function (motion) {
+                // Acceleration
+                var accelerationX = motion.acceleration.x;
+                var accelerationY = motion.acceleration.y;
+                var accelerationZ = motion.acceleration.z;
+
+                var accObj = { "accelerationX": accelerationX, "accelerationY": accelerationY, "accelerationZ": accelerationZ };
+                socket.emit('acceleration', accObj);
+
+            }, false);
+        } else {
+            console.log("error: Check sensorplug documentation");
+        }
+    };
+
+    var render = function() {
+        console.log(detectGeolocation());
+        console.log(detectOrientation());
+        console.log(detectAcceleration());
+    };
+
+    render();
+
     return {
-        detectOrientation: detectOrientation,
-        handleOrientation: handleOrientation
+        handleOrientation: handleOrientation,
+        handleGeolocation: handleGeolocation,
+        handleAcceleration: handleAcceleration
     };
 
 })();
