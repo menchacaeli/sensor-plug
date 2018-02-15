a835b9898c98d78d1d334ebfabdc52c450de7436
elimenchaca <menchacaeli@gmail.com>
2018-02-15 11:17:57 -0700

    Added comments to code

--- src/sensorplug.js
+++ src/sensorplug.js
@@ -1,8 +1,10 @@
+"use strict";
+
 /**
  * Created by elimenchaca on 11/6/17.
  */
-var Device = (function IIFE() {
-    var detectAcceleration = function() {
+var Device = function IIFE() {
+    var detectAcceleration = function detectAcceleration() {
         if (!('ondevicemotion' in window)) {
             return "sensorplug: Device Acceleration is not supported";
         } else {
@@ -10,15 +12,15 @@ var Device = (function IIFE() {
         }
     };
 
-    var detectOrientation = function() {
+    var detectOrientation = function detectOrientation() {
         if (!('ondeviceorientation' in window)) {
             return "sensorplug: Device Orientation is not supported";
         } else {
-            return "sensorplug: Device Orientation is supported!" ;
+            return "sensorplug: Device Orientation is supported!";
         }
     };
 
-    var detectGeolocation = function() {
+    var detectGeolocation = function detectGeolocation() {
         if (!("geolocation" in navigator)) {
             return "sensorplug: Device Geolocation is not supported.";
         } else {
@@ -26,7 +28,8 @@ var Device = (function IIFE() {
         }
     };
 
-    var handleOrientation = function () {
+    var handleOrientation = function handleOrientation() {
+			if (window.DeviceOrientation) {
         window.addEventListener('deviceorientation', function (eventData) {
             // gamma is the left-to-right tilt in degrees, where right is positive
             var gamma = eventData.gamma;
@@ -34,14 +37,19 @@ var Device = (function IIFE() {
             var beta = eventData.beta;
             // alpha is the compass direction the device is facing in degrees
             var alpha = eventData.alpha;
-        
-            var oriObj = {"gamma": gamma, "beta": beta, "alpha" : alpha};
+						
+						// creating an object to pass to the server-side
+            var oriObj = { "gamma": gamma, "beta": beta, "alpha": alpha };
+						
+						// emitting the oriObj to the server-side
             socket.emit('orientation', oriObj);
-        
         }, false);
+			} else {
+				console.log("error: Check sensorplug documentation");
+			}
     };
 
-    var handleGeolocation = function() {
+    var handleGeolocation = function handleGeolocation() {
         var positionOptions = {
             enableHighAccuracy: true,
             timeout: 10 * 1000, // 10 seconds
@@ -49,10 +57,15 @@ var Device = (function IIFE() {
         };
 
         function success(position) {
+						// specifies the north–south position of a point on the Earth's surface
             var latitude = position.coords.latitude;
+						// specifies the east-west position of a point on the Earth's surface
             var longitude = position.coords.longitude;
-
-            var geoObj = {"latitude": latitude, "longitude": longitude};
+						
+						// creating an object to pass to the server-side
+            var geoObj = { "latitude": latitude, "longitude": longitude };
+						
+						// emitting the oriObj to the server-side
             socket.emit('geolocation', geoObj);
         }
 
@@ -63,24 +76,26 @@ var Device = (function IIFE() {
         navigator.geolocation.getCurrentPosition(success, error, positionOptions);
     };
 
-    var handleAcceleration = function() {
+    var handleAcceleration = function handleAcceleration() {
         if (window.DeviceMotionEvent) {
             window.addEventListener('devicemotion', function (motion) {
-                // Acceleration
+                // provides acceleration data, in m/s² for each of the x, y, and z axes
                 var accelerationX = motion.acceleration.x;
                 var accelerationY = motion.acceleration.y;
                 var accelerationZ = motion.acceleration.z;
-
+								
+								// creating an object to pass to the server-side
                 var accObj = { "accelerationX": accelerationX, "accelerationY": accelerationY, "accelerationZ": accelerationZ };
+								
+								// emitting the oriObj to the server-side
                 socket.emit('acceleration', accObj);
-
             }, false);
         } else {
             console.log("error: Check sensorplug documentation");
         }
     };
 
-    var render = function() {
+    var render = function render() {
         console.log(detectGeolocation());
         console.log(detectOrientation());
         console.log(detectAcceleration());
@@ -93,6 +108,4 @@ var Device = (function IIFE() {
         handleGeolocation: handleGeolocation,
         handleAcceleration: handleAcceleration
     };
-
-})();
-
+}();
